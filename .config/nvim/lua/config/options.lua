5f6ab7a664b045f4e135d0c265163bfcf26c0da8
Filonzi Jacopo <jacopofilonzi@gmail.com>
2025-11-08 15:18:43 +0100

    added 4 spaces tabulation and no relative line number

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,8 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- not relative line numbers
+vim.opt.relativenumber = false
+
+-- Tab to 4 spaces
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
